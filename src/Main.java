fae85308b475c0607255a808e57b68d798c54869
Csönge Bence <bluebence@gmail.com>
2020-03-26 18:12:36 +0100

    Main hozzáadás

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Game game = new Game();
+		
+		String lista = "Választható forgatókönyvek:\n" +
+						"1. Valami";
+		
+		String bemenet = "";
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
+		while(!bemenet.equals("kilep")) {
+			System.out.println(lista);
+			bemenet = reader.readLine();
+			
+			switch(bemenet) {
+			case "1":
+				System.out.println("forgatókönyv 1");
+				break;
+			
+			}
+	        
+		}
+
+	}
+
+}
