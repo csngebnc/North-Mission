1aff8833c59780b32194094ea03d56961b1b7605
BuildTools <unconfigured@null.spigotmc.org>
2020-05-11 18:02:15 +0200

    fixed (2)

---
+++ src/Player/Character.java
@@ -0,0 +1,121 @@
+package Player;
+
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+
+import Core.Game;
+import Map.Field;
+import Visual.Viewable;
+/**
+ * A karaktereket reprezentáló osztály.
+ * Leszármazottai a Player és a PolarBear
+ * @author Zalan
+ */
+public abstract class Character implements Viewable{
+	
+	/**
+	 * A field amin a karakter aktuálisan tartózkodik.
+	 * @author Zalan
+	 */
+	protected Field field;
+	protected boolean isDrowning;
+	protected int stamina;
+	protected Image[] img;
+	
+	public void setDrowning(boolean b) 
+	{ 
+		isDrowning = b;
+		if(isDrowning)
+			stamina = 0;
+	}
+
+	/**
+	 * A karakter köre.
+	 * Leszármazottak saját maguk valósítják meg.
+	 * @author Zalan
+	 */
+	public abstract boolean doTurn(Game g, KeyEvent e);
+	
+	public abstract void move(KeyEvent e);
+	public abstract void startTurn(Game g);
+	/**
+	 * A karakter életének változtatása.
+	 * Leszármazottak saját maguk valósítják meg.
+	 * @param n a health változása.
+	 * @author Zalan
+	 */
+	public abstract void alterHealth(int n);
+	
+	/**
+	 * A karakter staminájának csökkentése.
+	 * Leszármazottak saját maguk valósítják meg.
+	 * @author Zalan
+	 */
+	public abstract void drainStamina();
+	
+	/**
+	 * A karakter megmentésre kerül fuldokló helyzetbõl.
+	 * Leszármazottak saját maguk valósítják meg.
+	 * @param f erre a fieldre kerül a megmentett karakter.
+	 * @author Zalan
+	 */
+	public abstract boolean save(Field f);
+	
+	/**
+	 * A karakter a paraméterként kapott karaktert összeütközteti saját magával.
+	 * Leszármazottak saját maguk valósítják meg.
+	 * @param c ezen karaktert ütközteti össze magával.
+	 * @author Zalan
+	 */
+	public abstract void collideWith(Character c);
+	
+	
+	/**
+	 * Karakter ütközése jegesmedvével.
+	 * Leszármazottak saját maguk valósítják meg.
+	 * @param pb a jegesmedve, akivel ütközött.
+	 * @author Zalan
+	 */
+	public void hitBy(PolarBear pb) {}
+	
+	/**
+	 * Karakter ütközése játékossal.
+	 * Leszármazottak saját maguk valósítják meg.
+	 * @param pl a játékos, akivel ütközött.
+	 * @author Zalan
+	 */
+	public void hitBy(Player pl) {}
+	
+	
+	/**
+	 * Karakter mezõjének beállítása.
+	 * @param f a field új értéke.
+	 * @author Zalan
+	 */
+	public void setField(Field f) {
+		field = f;
+	}
+	
+	/**
+	 * Karakter mezõjének lekérdezése.
+	 * @author Zalan
+	 */
+	public Field getField() {
+		return field;
+	}
+	
+	/**
+	 * Karakter nevének lekérdezése.
+	 * Leszármazottak saját maguk valósítják meg.
+	 * @author Zalan
+	 */
+	public abstract String getName();
+	
+	
+	/**
+	 * Teszteléshez
+	 */
+	public void Properties()
+	{
+	}
+}
---
+++ src/Player/PolarBear.java
@@ -0,0 +1,129 @@
+package Player;
+
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import Player.Direction;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+
+import Core.Game;
+import Map.Field;
+import Visual.View;
+
+/**
+ * A jegesmedvét reprezentáló osztály, ami a Character leszármazottja.
+ * @author Zalan
+ */
+public class PolarBear extends Character{
+	/**
+	 * A jegesmedve karakter egy random szomszédos mezõre lép a körében.
+	 * @author Zalan
+	 */
+	public PolarBear() {
+		super();
+		img = new Image[2];
+		img[0]= new ImageIcon("./assets/characters/polar_standing.png").getImage();
+		img[1] = new ImageIcon("./assets/characters/polar_drowning.png").getImage();
+	}
+	@Override
+	public boolean doTurn(Game g, KeyEvent e) {
+		move(null);
+		g.notifyView();
+		g.nextCharacter();
+		return true;
+	}
+	
+	public void startTurn(Game g) {
+		doTurn(g, null);
+	}
+	
+	public void move(KeyEvent e) {
+		ArrayList<Integer> nexts = new ArrayList<Integer>();
+		for(int i=0; i< field.getNeighbours().size();i++) {
+			if(field.getNeighbours().get(i)!=null) {
+				nexts.add(i);
+			}
+		}
+		int max = nexts.size()-1;
+		int where = nexts.get((int)(Math.random()*max));
+		System.out.println(where);
+		field.moveMeTo(this, Direction.FromInt(where));
+	}
+
+	/**
+	 * Ez a metódus nem csinál semmit, a PolarBear-nek nem kell az életét állítani.
+	 * @author Zalan
+	 */
+	@Override
+	public void alterHealth(int n) {
+	}
+	
+	/**
+	 * Ez a metódus nem csinál semmit, a PolarBear-nek nem kell a stamináját állítani.
+	 * @author Zalan
+	 */
+	@Override
+	public void drainStamina() {		
+	}
+	
+	/**
+	 * Ez a metódus false-szal tér vissza, hiszen egy Jegesmedvét nem kell kimentenünk ha lyukba esett.
+	 * @author Zalan
+	 */
+	@Override
+	public boolean save(Field f) {
+		return false;
+	}
+
+	/**
+	 * A jegesmedve a paraméterként kapott karaktert összeütközteti saját magával.
+	 * @param c a karakter akivel a jegesmedve összeütközteti magát.
+	 * @author Zalan
+	 */
+	@Override
+	public void collideWith(Character c) {
+		c.hitBy(this);
+	}
+
+	/**
+	 * Ha a jegesmedve játékossal ütközött, akkor a játékot elveszítették a játékosok.
+	 * @param p a játékos, akivel a jegesmedve összeütközött.
+	 * @author Zalan
+	 */
+	@Override
+	public void hitBy(Player p) {
+		Game.loseGame();		
+	}
+	
+	/**
+	 * Teszteléshez
+	 */
+	@Override
+	public void Properties()
+	{
+		System.out.println("Jegesmedve");
+	}
+	
+	/**
+	 * A jegesmedve nevének lekérdezése.
+	 * @author Zalan
+	 */
+	public String getName() {
+		return "Jegesmedve";
+	}
+
+	@Override
+	public void draw(View v) {
+		if(isDrowning) {
+			v.drawThing(field.GetX()+32, field.GetY()+14, img[1]);
+		}else {
+			if(field.hasBuilding())
+				v.drawThing(field.GetX()+25, field.GetY()+10, img[0]);	
+			else
+				v.drawThing(field.GetX()+25, field.GetY(), img[0]);	
+		}	
+	}
+
+}
